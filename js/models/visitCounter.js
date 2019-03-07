8e88b3f437f7350ae99aeb90b30a5c24f5575d50
Yavor Grancharov <bearhold@gmail.com>
2019-03-07 17:31:52 +0200

    fixed database security issues

---
+++ js/models/visitCounter.js
@@ -0,0 +1,23 @@
+let visitCounter = (() => {
+    function visitCounter() {
+        $(document).ready(function () {
+            let count = 0;
+            let post_id = $('#id').val();
+            console.log(post_id)
+            commentService.countVisitsPost(post_id, count).then((res) => {
+                commentService.countVisitsGet().then((visits) => {
+                    visits.forEach(function (visit, index) {
+                        if (visit.post_id === post_id) {
+                            count++;
+                        }
+                    })
+                    $('.post-views')[0].innerHTML = count + ' Views'
+                })
+            })
+        })
+    }
+
+    return {
+        visitCounter
+    }
+})();
