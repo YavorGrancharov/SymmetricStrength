dc472988dbb5edf5b310bb389054ddee07a6eb25
Yavor Grancharov <bearhold@gmail.com>
2019-03-08 17:12:06 +0200

    testing google recaptcha

--- js/controller/subscribeController.js
+++ js/controller/subscribeController.js
@@ -12,7 +12,7 @@ let subscribeController = (() => {
                         return;
                     }
                     grecaptcha.ready(function() {
-                        grecaptcha.execute('6LcKZJYUAAAAAA1J1y6UDRb5SVsb8HS9uq4TJSGn', {action: 'validate_captcha'}).then(function(token) {
+                        grecaptcha.execute('6LcKZJYUAAAAAA1J1y6UDRb5SVsb8HS9uq4TJSGn', {action: 'subscribe_email'}).then(function(token) {
                             $('.box-subscribe').prepend('<input type="hidden" name="g-recaptcha-response" value="' + token + '">');
                             $.post($('#subscribe_email'),{date: date, email: email, token: token}, function (result) {
                                 if(result.success) {
