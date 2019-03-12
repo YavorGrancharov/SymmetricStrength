61b500270377f2b08df3287caaaf2f1db85978ce
Yavor Grancharov <bearhold@gmail.com>
2019-03-12 15:44:29 +0200

    recaptcha testing

--- js/controller/subscribeController.js
+++ js/controller/subscribeController.js
@@ -3,13 +3,9 @@ let subscribeController = (() => {
         $(document).ready(function () {
             $('#subscribe_email').keydown(function (e) {
                 let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
-                //let response = grecaptcha.getResponse();
+                let response = grecaptcha.getResponse();
                 let date = formatDate(new Date());
                 let email = $('#subscribe_email').val();
-                let validateCaptcha = function (response) {
-                    $('#subscribe_hiddenRecaptcha').val(response);
-                };
-                let response = grecaptcha.render('subscribe_captcha', {'sitekey' : '6Ldg2pYUAAAAAJAvcm5rIdhZaU3kpydbkoDHm5uP', 'callback' : validateCaptcha});
                 if (e.which === 13) {
                     if (!validEmail.test(String(email).toLowerCase())) {
                         alert('Въведете валидна е-поща!');
