20d2147a2f40fb57ebee5a63f805f388fd530ade
YavorGrancharov <bearhold@gmail.com>
2019-11-30 19:42:33 +0200

    strength controller test

--- js/controller/strengthController.js
+++ js/controller/strengthController.js
@@ -635,18 +635,18 @@ let strengthController = (() => {
                     gm[i].innerHTML = (Math.round((femaleRealDeadlift * 0.44 * strengthIndexes[i - 1]) * getIntensity(good_morning_reps))).toString();
                 }
             }
-            for (let i = 0; i < userInput.length; i++) {
-                if (userInput[i].value.value) {
-                    fill.addEventListener("click", function (event) {
-                        event.preventDefault();
-                        userInput[i].value.value = Math.round(((relativeDeadlift * getIntensity(idealSet[i].reps.value)) * movementsIndexes[i].value));
-                    })
-                    fill2.addEventListener("click", function (event) {
-                        event.preventDefault();
-                        userInput[i].value.value = Math.round((nextLevelDeadlift * getIntensity(userInput[i].reps.value)) * movementsIndexes[i].value);
-                    })
-                }
-            }
+            // for (let i = 0; i < userInput.length; i++) {
+            //     if (userInput[i].value.value) {
+            //         fill.addEventListener("click", function (event) {
+            //             event.preventDefault();
+            //             userInput[i].value.value = Math.round(((relativeDeadlift * getIntensity(idealSet[i].reps.value)) * movementsIndexes[i].value));
+            //         })
+            //         fill2.addEventListener("click", function (event) {
+            //             event.preventDefault();
+            //             userInput[i].value.value = Math.round((nextLevelDeadlift * getIntensity(userInput[i].reps.value)) * movementsIndexes[i].value);
+            //         })
+            //     }
+            // }
             console.log(labels)
 
             let idealChartBtn = document.getElementById("idealChartBtn");
